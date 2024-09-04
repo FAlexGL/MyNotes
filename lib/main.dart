8d270c790605ced5db61c27636d7269f7868a66b
FAlexGL <f.alejandro.gl@hotmail.com>
2024-09-04 17:30:31 +0200

    first commit, advanced project state

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/notes/create_update_note_view.dart';
+import 'package:mynotes/views/notes/notes_view.dart';
+import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
+
+void main() {
+  //Permite el uso de widgets "futuros" (como el que se debería usar para inicializar Firebase)
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+      routes: {
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
+        verifyEmailRoute: (context) => const VerifyEmailView(),
+        CreateOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
+      },
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      //Inicializa Firebase, si falla, no se genera el widget
+      future: AuthService.firebase().initialize(),
+      builder: (context, snapshot) {
+        //snapshot incluye el estado del Futuro solicitado (en este caso, el Future es la inicialización de Firebase: "Firebase.initalizeApp(...)")
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = AuthService.firebase().currentUser;
+            if (user != null) {
+              if (user.isEmailVerified) {
+                return const NotesView();
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
---
+++ lib/services/auth/auth_user.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart' show User;
+import 'package:flutter/foundation.dart';
+
+@immutable
+class AuthUser {
+  final bool isEmailVerified;
+  final String? email;
+  const AuthUser({
+    required this.email,
+    required this.isEmailVerified,
+  });
+
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+        isEmailVerified: user.emailVerified,
+        email: user.email,
+      );
+}
